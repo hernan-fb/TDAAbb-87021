d3a1d7fd9563ecfab01ebf97eb28d3956a901cd1
Hernan Fernandez Brando <hfbrando@NGCSI018.casads.com.ar>
2023-05-15 03:26:52 -0300

    bug fix

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -3,9 +3,10 @@
 //typedef enum { INORDEN, PREORDEN, POSTORDEN } abb_recorrido;
 
 /* Función auxiliar para imprimir elementos */
-void imprimir_elemento(void *elemento, void *aux) {
+bool imprimir_elemento(void *elemento, void *aux) {
     int *elem = (int*) elemento;
     printf("%d - ", *elem);
+	return true;
 }
 
 /* Función auxiliar para comparar elementos */
@@ -31,15 +32,18 @@ int main() {
     int elem2 = 2;
     int elem3 = 3;
     int elem4 = 4;
-
+    printf("Inserto el elmento: 2\n");
     arbol = abb_insertar(arbol, &elem2);
+    printf("Inserto el elmento: 1\n");
     arbol = abb_insertar(arbol, &elem1);
+    printf("Inserto el elmento: 4\n");
     arbol = abb_insertar(arbol, &elem4);
+    printf("Inserto el elmento: 3\n");
     arbol = abb_insertar(arbol, &elem3);
 
     /* Verificar tamaño y si está vacío */
     printf("Tamaño del árbol: %zu\n", abb_tamanio(arbol));
-    printf("El árbol está vacío: %d\n", abb_vacio(arbol));
+    printf("El árbol está vacío: %s\n", abb_vacio(arbol)?"true":"false");
 
     /* Buscar elementos */
     int *elem_buscado = abb_buscar(arbol, &elem3);
@@ -51,11 +55,11 @@ int main() {
 
     /* Recorrer el árbol con una función auxiliar */
     printf("Recorrido en orden del árbol: ");
-    abb_con_cada_elemento(arbol, INORDEN, imprimir_elemento, NULL);
+    abb_con_cada_elemento(arbol, PREORDEN, imprimir_elemento, NULL);
     printf("\n");
 
     /* Eliminar elementos */
-    arbol = abb_quitar(arbol, &elem3);
+    abb_quitar(arbol, &elem3);
     printf("Tamaño del árbol después de eliminar un elemento: %zu\n", abb_tamanio(arbol));
 
     /* Destruir el árbol */
